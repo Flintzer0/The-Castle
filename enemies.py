51357b648f7337d790b7c883a75ff8645e65d2a9
Stephen Bell <stephenbell@tcsemi.com>
2023-07-31 15:58:24 -0700

    Added monster-type subclasses, added to __str__ as well.

--- enemies.py
+++ enemies.py
@@ -39,7 +39,7 @@ class Enemy:
         return self.name
 
     def __str__(self):
-        return "{}\n========================================\n{}========================================\nHP: {}\nDamage: {}\nWeakness: {}\nDEF: {}\nRES: {}\nSPD: {}\nSKL: {}\nLUCK: {}\n".format(self.name, self.description, self.mhp, self.damage, self.weak, self.DEF, self.RES, self.SPD, self.SKL, self.LUCK)
+        return "{}     Type: {}\n========================================\n{}========================================\nHP: {}     Weakness: {}\nDMG: {}     DEF: {}\nRES: {}     SPD: {}\nSKL: {}     LUCK: {}\n".format(self.name, self.ID, self.description, self.mhp, self.weak, self.damage, self.DEF, self.RES, self.SPD, self.SKL, self.LUCK)
  
     def is_alive(self):
         return self.hp > 0
@@ -60,16 +60,47 @@ class Enemy:
             time.sleep(.2)
 
 class Undead(Enemy):
-    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description): 
+    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+        self.ID = "Undead"
+        super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
+
+class Beast(Enemy):
+    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+        self.ID = "Beast"
+        super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
+
+class Bug(Enemy):
+    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+        self.ID = "Bug"
+        super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
+
+class Humanoid(Enemy):
+    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+        self.ID = "Humanoid"
+        super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
+
+class Demon(Enemy):
+    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+        self.ID = "Demon"
+        super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
+
+class Dragon(Enemy):
+    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+        self.ID = "Dragon"
+        super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
+
+class Shifter(Enemy):
+    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+        self.ID = "Shifter"
         super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
 
 class Boss(Enemy):
-    def __init__(self, name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
+    def __init__(self, name, hp, mhp, damage,type, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description):
         super().__init__(name, hp, mhp, damage, statusatk, status_chance, weak, DEF, RES, SPD, SKL, LUCK, EXP, gold, part, description)
 
 # Tier 1 Enemies
 
-class giant_spider(Enemy):
+class giant_spider(Bug):
     import items
     steal_list = {
         '1': items.gold(100),
@@ -80,9 +111,9 @@ class giant_spider(Enemy):
     def __init__(self):
         import items
         super().__init__(name="Giant Spider", hp=10, mhp=10, damage=2, statusatk=None, status_chance=0, weak=None, DEF=0, RES=1, SPD=3, SKL=2, LUCK=1, EXP=13, gold=10, part=items.spider_leg(1),
-                         description="A large spider with dripping fangs and a hairy body. They are quicker than most basic monsters. \nBeing all buggy, its defense isn't superb, but it has a bit of magic resistance.\n")
+                         description="A large spider with dripping fangs and a hairy body.\nThey are quicker than most basic monsters. Being all buggy, its defense\nisn't superb, but it has a bit of magic resistance.\n")
  
-class goblin(Enemy):
+class goblin(Humanoid):
     import items
     steal_list = {
         '1': items.gold(100),
@@ -92,8 +123,22 @@ class goblin(Enemy):
 
     def __init__(self):
         import items
-        super().__init__(name="Goblin", hp=5, mhp=5, damage=1, statusatk=None, status_chance=0, weak="Cold", DEF=1, RES=0, SPD=2, SKL=1, LUCK=0, EXP=10, gold=16, part=items.goblin_ear(1),
-                         description="A small, green humanoid with a large nose and pointed ears, wielding a wooden club. \nThey aren't very special, but they have a bit of defense.\n")
+        super().__init__(name="Goblin",
+                         hp=5,
+                         mhp=5,
+                         damage=1,
+                         statusatk=None,
+                         status_chance=0,
+                         weak="Cold",
+                         DEF=1,
+                         RES=0,
+                         SPD=2,
+                         SKL=1,
+                         LUCK=0,
+                         EXP=10,
+                         gold=16,
+                         part=items.goblin_ear(1),
+                         description="A small, green humanoid with a large nose and pointed ears.\nThey aren't very special, but they have a bit of defense.\n")
 
 class skeleton(Undead):
     import items
@@ -105,7 +150,7 @@ class skeleton(Undead):
 
     def __init__(self):
         import items
-        super().__init__(name="Skeleton", 
+        super().__init__(name="Skeleton",
                          hp=7, 
                          mhp=7, 
                          damage=3, 
@@ -120,9 +165,9 @@ class skeleton(Undead):
                          EXP=17, 
                          gold=13, 
                          part=items.femur_bone(1),
-                         description="A walking skeleton. \nThey are slow, but they have some defense and deal a little more \ndamage.\n")
+                         description="A walking skeleton. \nThey are slow, but they have some defense and deal a little more \ndamage. Very weak to Magic damage.\n")
         
-class large_rat(Enemy):
+class large_rat(Beast):
     import items
     steal_list = {
         '1': items.gold(100),
@@ -133,9 +178,9 @@ class large_rat(Enemy):
     def __init__(self):
         import items
         super().__init__(name="Large Rat", hp=15, mhp=10, damage=3, statusatk="Disease", status_chance=30, weak="Fire", DEF=1, RES=1, SPD=1, SKL=0, LUCK=0, EXP=15, gold=15, part=items.rat_tail(1),
-                         description="An incredibly large rat. They aren't hard to hit, but their thick hide and hard teeth \ngive them decent health and damage.\n")
+                         description="An incredibly large rat.\nThey aren't hard tp hurt, but their thick hide and hard teeth give them\ndecent health and damage.\n")
 
-class demon_bat(Enemy):
+class demon_bat(Demon):
     import items
     steal_list = {
         '1': items.gold(100),
@@ -159,10 +204,13 @@ class zombie(Undead):
     def __init__(self):
         import items
         super().__init__(name="Zombie", hp=15, mhp=15, damage=3, statusatk="Disease", status_chance=45, weak="", DEF=2, RES=2, SPD=1, SKL=0, LUCK=0, EXP=20, gold=20, part=items.rotting_flesh(1),
-                         description="A walking corpse. They are slow, but they have some defense and deal a little more \ndamage.\n")
+                         description="A walking corpse.\nThey are slow, but they have some defense and deal a little more damage.\nIt is important to strike them down quickly to avoid being afflicted\nwith DISEASE.\n")
 
-# Bosses   
-class giant_centipede(Boss):
+# Bosses
+# BF2 Boss
+
+# BF1 Boss
+class giant_centipede(Boss, Bug):
     # Basement Floor Boss
     import items
 
@@ -176,6 +224,6 @@ class giant_centipede(Boss):
 
     def __init__(self):
         import items
-        super().__init__(name="Centipede", hp=20, mhp=20, damage=5, statusatk="Poison", status_chance=50, weak="Fire", DEF=3, RES=2, SPD=5, SKL=4, LUCK=2, EXP=50, gold=30, part=items.centipede_carapace(1),
-                         description="A giant centipede, with acid dripping off its mandibles. There also appeared to be several old weapons stuck in its hard shell. \nBeing a centipede, it is quite dangerous, with high damage, some defense both physically and magically, \nquick, and capable of making skillful strikes. Only engage when you are ready.\n")
+        super().__init__(name="Centipede", hp=20, mhp=20, damage=5, type="", statusatk="Poison", status_chance=50, weak="Fire", DEF=3, RES=2, SPD=5, SKL=4, LUCK=2, EXP=50, gold=30, part=items.centipede_carapace(1),
+                         description="A giant centipede, with acid dripping off its mandibles. There also \nappears to be several old weapons stuck in its hard shell. Being a \ncentipede, it is quite dangerous, with high damage, some defense both \nphysically and magically, quick, and capable of making skillful \nstrikes. Only engage when you are ready.\n")
         
